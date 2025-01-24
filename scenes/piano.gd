4bfa6fe1a3c4e7da0d73e5a6e82cf68a48d10238
Kori / William <william.pinel987@gmail.com>
2025-01-24 22:41:24 +0100

    Update inventory

--- scenes/piano.gd
+++ scenes/piano.gd
@@ -5,6 +5,8 @@ var attackSize = 1.0
 @export var hp = 1
 @export var speed = 100
 @export var damage = 10
+@export var baseAmmo = 1
+@export var attackSpeed = 7.5
 
 var target = Vector2.ZERO  # target est déjà un Vector2
 var angle = Vector2.ZERO
@@ -14,12 +16,7 @@ var angle = Vector2.ZERO
 func _ready() -> void:
 	angle = global_position.direction_to(target)
 	rotation = angle.angle()
-	match level:
-		1:
-			hp = 1
-			speed = 100
-			damage = 10
-			attackSize = 1.0
+	loadLevel()
 
 func _physics_process(delta: float) -> void:
 	# Vérifie si la distance entre position et target est très petite (pour éviter des problèmes de précision)
@@ -46,3 +43,13 @@ func _on_timer_timeout() -> void:
 
 func _on_explosion_timer_timeout() -> void:
 	queue_free()
+
+func loadLevel():
+	match level:
+		1:
+			hp = 1
+			speed = 100
+			damage = 10
+			attackSize = 1.0
+			baseAmmo = 1
+			attackSpeed = 7.5
