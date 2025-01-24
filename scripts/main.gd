f252c0a7129bbf314c6cad24ea733e7a8d85d62d
Odaxty <theochauv@gmail.com>
2025-01-24 11:37:12 +0100

    fix merge théo

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,19 @@
 extends Node2D
 
-
-func _ready() -> void:
-	pass 
-
+@onready var camera = get_node("Player/Camera2D")
 
 func _process(delta: float) -> void:
-	pass
+	$Path2D.position = camera.global_position
+	$Path2D.position -= Vector2(500, 350)
+
+func spawn_mobs():
+	var new_mob = preload("res://scenes/enemy.tscn").instantiate()
+	
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	
+	add_child(new_mob)
+
+
+func _on_timer_timeout() -> void:
+	spawn_mobs()
--- scripts/sceneStart.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _on_start_pressed_start():
-	get_tree().change_scene_to_file("res://scenes/Main.tscn")
-
-
-func _on_settings_pressed():
-	get_tree().change_scene_to_file("res://scenes/scene_settings.tscn")
